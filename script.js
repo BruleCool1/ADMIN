e867fde4999bb39b7fc9a106477c597a4c94f853
BruleCool1 <152447228+BruleCool1@users.noreply.github.com>
2024-09-08 08:21:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 // Sample user data
 const users = {
     "ADMIN": "6612",
-    "ADMIN1": "TEST",
-    "user2": "password2",
-    "user3": "password3"
-};
+    };
 
 // Function to validate login
 function validateLogin(username, password) {
